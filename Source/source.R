f438f9ef1a4ce6f16af30464cadc39b199fe51b6
junbangg <wnstjr4620@naver.com>
2020-12-21 18:06:33 +0900

    Added new photos

--- Source/source.R
+++ Source/source.R
@@ -1,8 +1,12 @@
 library(dplyr)
 library(tidyverse) 
 library(gridExtra)
+library(ggpubr)
+library(broom)
+library(AICcmodavg)
 require(ggplot2)
 require(stringr)
+require(dplyr)
 #read data
 data <- read.csv("../Data/games.csv")
 head(data, 5)
@@ -59,3 +63,63 @@ data %>%
   labs(y = "Frequency", x = "Opening", title = "Best Openings for White")+
   scale_fill_gradient(low = "gray", high = "darkgray")+
   theme(axis.text = element_text(size = 15),axis.title = element_text(size = 15))
+
+#
+rating_white <- data %>% filter(winner == "white") %>% select(winner, white_rating, black_rating)
+rating_black <- data %>% filter(winner == "black") %>% select(winner, white_rating, black_rating)
+
+rating_white_wins <- rating_white[sample(nrow(rating_white),5000),]
+rating_black_wins <- rating_white[sample(nrow(rating_white),5000),]
+#shapiro
+shapiro.test(rating_white_wins$white_rating)
+shapiro.test(rating_white_wins$black_rating) 
+shapiro.test(rating_black_wins$white_rating)
+shapiro.test(rating_black_wins$black_rating) 
+#pearson
+cor.test(rating_white_wins$white_rating, rating_white_wins$black_rating, method = "pearson")
+cor.test(rating_black_wins$white_rating, rating_black_wins$black_rating, method = "pearson")
+#kendall
+cor.test(rating_white_wins$white_rating, rating_white_wins$black_rating, method = "kendall")
+cor.test(rating_black_wins$white_rating, rating_black_wins$black_rating, method = "kendall")
+#spearman
+hypo1 <- cor.test(rating_white_wins$white_rating, rating_white_wins$black_rating, method = "spearman")
+hypo2 <- cor.test(rating_black_wins$white_rating, rating_black_wins$black_rating, method = "spearman")
+hypo1
+hypo2
+
+options(repr.plot.width = 15, repr.plot.height = 8)
+
+ggplot(rating_white_wins, aes(white_rating, black_rating, label = 'W', color = winner))+
+  geom_text()+
+  theme_minimal()+
+  theme(legend.position = "bottom")+
+  geom_jitter(alpha = 0.3, size = 2.5, width = 0.3, height = 0.3)+
+  geom_smooth(method = "lm", color = "red", lty = 2, se = FALSE, size = 0.6)+
+  scale_color_manual(values = c("darkblue","steelblue"))+
+  labs(title = paste("Spearman Correlation Coefficient:", round(hypo1$estimate, digits = 2)))
+
+ggplot(rating_black_wins, aes(white_rating, black_rating, label = 'B', color = winner))+
+  geom_text()+
+  theme_minimal()+
+  theme(legend.position = "bottom")+
+  geom_jitter(alpha = 0.3, size = 2.5, width = 0.3, height = 0.3)+
+  geom_smooth(method = "lm", color = "blue", lty = 2, se = FALSE, size = 0.6)+
+  scale_color_manual(values = c("darkred","steelblue"))+
+  labs(title = paste("Spearman Correlation Coefficient:", round(hypo2$estimate, digits = 2)))
+
+#Anova
+opening_white <- data %>%
+  mutate(firstMove = str_sub(moves, 1, 2)) %>%
+  filter(winner == "white") %>%
+  select(winner, white_rating, black_rating, opening_name, firstMove) %>%
+
+opening_black <- data %>%
+  mutate(firstMove = str_sub(moves, 1, 2)) %>%
+  filter(winner == "black") %>% 
+  select(winner, white_rating, black_rating, opening_name, firstMove)
+
+when_white_wins <- aov(black_rating ~ white_rating, data = opening_white)
+summary(when_white_wins)
+
+when_black_wins <- aov(white_rating ~ black_rating, data = opening_white)
+summary(when_black_wins)
